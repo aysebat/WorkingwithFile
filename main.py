6455cd7e01b458685fcf578ece57350dc5e782b6
Ayse Bat <110904026+aysebat@users.noreply.github.com>
2023-03-22 22:13:43 +0000

    Created rename with folder function

--- main.py
+++ main.py
@@ -12,11 +12,31 @@ def rename_file(name="new"):
   for path in file_paths:
     print("Current Files in the Path")
     print(path)
-    new_file_name = name+ path.stem +path.suffix
+    new_file_name = name +'-'+ path.stem +path.suffix
     new_file_path = path.with_name(new_file_name)
     print("New File Path")
     print(new_file_path)
     path.rename(new_file_path)
     print("--------------------------")
 
-rename_file(name="abat-")
+def rename_with_folder():
+  root_dir = Path('files2')
+  #take all folder and files
+  file_path =  root_dir.glob("**/*")
+  for path in file_path:
+    #check the path has file and continue
+    if path.is_file():
+      #take aprt the parts and takes  lates -2 as a folder name
+      folder_name = path.parts[-2]
+      new_file_name = folder_name +'-'+ path.name
+      new_folder_name = path.with_name(new_file_name)
+      path.rename(new_folder_name)
+    
+#rename_file(name="abat-")
+#rename_with_folder()
+
+
+  
+
+
+
