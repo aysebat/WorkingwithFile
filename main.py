ded656b2d31de854d859d2f5426ace396598caa1
Ayse Bat <110904026+aysebat@users.noreply.github.com>
2023-03-23 09:05:06 +0000

    rename_file_with_date function created

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from datetime import datetime
 
 def rename_file(name="new"):
   """This takes the argument that provided and the files as an prefix"""
@@ -46,7 +47,29 @@ def rename_sub_sub_folder():
       new_folder_name = path.with_name(new_file_name)
       print(f"Renamed file path \n  {new_folder_name}")
       path.rename(new_folder_name)
+
+def rename_file_with_date():
+  """When the function run it add the todays date infront of the file"""
+
+  root_dir =Path('files')
+  file_paths = root_dir.iterdir()
   
+  print(f"Current Working Directory: {Path.cwd()}")
+  print("--------------------------")
+  
+  for path in file_paths:
+    print("Current Files in the Path")
+    print(path)
+    now =  datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    new_file_name =  now +'-'+ path.stem +path.suffix
+    new_file_path = path.with_name(new_file_name)
+    print("New File Path")
+    print(new_file_path)
+    path.rename(new_file_path)
+    print("--------------------------")
+
+
+rename_file_with_date()
 #rename_file(name="abat-")
 #rename_with_folder()
 #rename_sub_sub_folder()
