ee71050c231926a808dee65740678a236a154fad
Ayse Bat <110904026+aysebat@users.noreply.github.com>
2023-03-24 03:00:54 +0000

    create_empty_files function created

--- main.py
+++ main.py
@@ -5,71 +5,72 @@ from datetime import datetime
 def rename_file(name="new"):
   """This takes the argument that provided and the files as an prefix"""
 
-  root_dir =Path('files')
+  root_dir = Path('files')
   file_paths = root_dir.iterdir()
-  
+
   print(f"Current Working Directory: {Path.cwd()}")
   print("--------------------------")
-  
+
   for path in file_paths:
     print("Current Files in the Path")
     print(path)
-    new_file_name = name +'-'+ path.stem +path.suffix
+    new_file_name = name + '-' + path.stem + path.suffix
     new_file_path = path.with_name(new_file_name)
     print("New File Path")
     print(new_file_path)
     path.rename(new_file_path)
     print("--------------------------")
 
+
 def rename_with_folder():
   root_dir = Path('files2')
   #take all folder and files
-  file_path =  root_dir.glob("**/*")
+  file_path = root_dir.glob("**/*")
   for path in file_path:
     #check the path has file and continue
     if path.is_file():
       #take aprt the parts and takes  lates -2 as a folder name
       folder_name = path.parts[-2]
-      new_file_name = folder_name +'-'+ path.name
+      new_file_name = folder_name + '-' + path.name
       new_folder_name = path.with_name(new_file_name)
       path.rename(new_folder_name)
 
+
 def rename_sub_sub_folder():
   """THIS FUNCTION RENAME WITH TWO SUB DIRECTORIES"""
-  root_dir= Path('files3')
+  root_dir = Path('files3')
   #go into two sun directory
-  file_path = root_dir.glob("**/**/*") 
+  file_path = root_dir.glob("**/**/*")
   for path in file_path:
     #do somethig if the it is file skip the just folder
     if path.is_file():
       print(f"Existing files and path \n {path}")
-      folder_names = path.parts[-3] +"-"+  path.parts[-2]
-      new_file_name= folder_names+'-'+path.name
+      folder_names = path.parts[-3] + "-" + path.parts[-2]
+      new_file_name = folder_names + '-' + path.name
       new_folder_name = path.with_name(new_file_name)
       print(f"Renamed file path \n  {new_folder_name}")
       path.rename(new_folder_name)
 
+
 def rename_file_with_date():
   """When the function run it add the todays date infront of the file"""
 
-  root_dir =Path('files')
+  root_dir = Path('files')
   file_paths = root_dir.iterdir()
-  
+
   print(f"Current Working Directory: {Path.cwd()}")
   print("--------------------------")
-  
+
   for path in file_paths:
     print("Current Files in the Path")
     print(path)
-    now =  datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-    new_file_name =  now +'-'+ path.stem +path.suffix
+    now = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    new_file_name = now + '-' + path.stem + path.suffix
     new_file_path = path.with_name(new_file_name)
     print("New File Path")
     print(new_file_path)
     path.rename(new_file_path)
     print("--------------------------")
-
-
 
 
 def convert_timestamp_to_time(given_time):
@@ -81,24 +82,26 @@ def convert_timestamp_to_time(given_time):
 def rename_folder_with_created_datetime():
   root_dir = Path('files2')
   #take all folder and files
-  file_path =  root_dir.glob("**/*")
+  file_path = root_dir.glob("**/*")
   for path in file_path:
     #check the path has file and continue
     if path.is_file():
       created_time = path.stat().st_ctime
-      date_created_str = convert_timestamp_to_time(created_time )
-      new_file_name = date_created_str +'-'+ path.name
+      date_created_str = convert_timestamp_to_time(created_time)
+      new_file_name = date_created_str + '-' + path.name
       new_folder_name = path.with_name(new_file_name)
       path.rename(new_folder_name)
 
-def rename_the_suffix_of_file(suffix = "csv"):
+
+def rename_the_suffix_of_file(suffix="csv"):
   root_dir = Path('files4')
   file_path = root_dir.iterdir()
   for path in file_path:
-    new_file_name = path.parts[1].split(".")[0] +"."+ suffix
-    new_folder_name = path.with_name(new_file_name )
+    new_file_name = path.parts[1].split(".")[0] + "." + suffix
+    new_folder_name = path.with_name(new_file_name)
     path.rename(new_folder_name)
-    
+
+
 def change_the_suffix(cur_suffix, new_suffix):
   root_dir = Path('files4')
   #rglob searh for all sub folder
@@ -106,16 +109,24 @@ def change_the_suffix(cur_suffix, new_suffix):
     if path.is_file():
       name_with_new_suffix = path.with_suffix(new_suffix)
       path.rename(name_with_new_suffix)
-    
+
+
+def create_empty_files():
+  """Create a empty txt files ranging from 10 to 20 and named with the same number"""
+  root_dir = Path('files5')
+
+  #loop over from 10 to the 20
+  for i in range(10, 21):
+    file_name = str(i) + '.txt'
+    file_path = root_dir / Path(file_name)
+    file_path.touch()
+
+
+#create_empty_files()
 #change_the_suffix(".txt",".csv")
-
-
 #rename_the_suffix_of_file("csv")
-
-
 #rename_file_with_date()
 #rename_file(name="abat-")
 #rename_with_folder()
 #rename_sub_sub_folder()
-#rename_folder_with_created_datetime()  
-
+#rename_folder_with_created_datetime()
